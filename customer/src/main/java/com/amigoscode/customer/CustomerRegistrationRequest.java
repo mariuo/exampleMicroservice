342b4e235db2ffe9c7d13b0f6c1fb94fbac591db
Mario Camelo <mariuo@gmail.com>
2022-11-10 16:52:19 -0500

    Projet created

---
+++ customer/src/main/java/com/amigoscode/customer/CustomerRegistrationRequest.java
@@ -0,0 +1,5 @@
+package com.amigoscode.customer;
+
+public record CustomerRegistrationRequest(String firstName, String lastName, String email) {
+
+}
---
+++ customer/src/main/java/com/amigoscode/customer/CustomerService.java
@@ -0,0 +1,32 @@
+package com.amigoscode.customer;
+
+import com.amigoscode.clients.fraud.FraudCheckResponse;
+import com.amigoscode.clients.fraud.FraudClient;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class CustomerService {
+    private final CustomerRepository customerRepository;
+//    private final RestTemplate restTemplate;
+    private final FraudClient fraudClient;
+    public void registerCustomer(CustomerRegistrationRequest request) {
+        Customer customer = Customer.builder()
+                .firstName(request.firstName())
+                .lastName(request.lastName())
+                .email(request.email())
+                .build();
+
+        customerRepository.saveAndFlush(customer);
+
+//        FraudCheckResponse fraudCheckResponse = restTemplate.getForObject("http://FRAUD/api/v1/fraud-check/{customerId}",
+//                FraudCheckResponse.class,
+//                customer.getId());
+        FraudCheckResponse fraudCheckResponse = fraudClient.isFraudster(customer.getId());
+
+        if(fraudCheckResponse.isFraudster()){
+            throw new IllegalStateException("Fraudster");
+        }
+    }
+}
